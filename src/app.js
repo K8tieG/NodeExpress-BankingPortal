be64bbabc8dd1c71560f6652ac8a17aa4f744660
Katie Gardner <kmgardn3r@gmail.com>
2021-09-08 15:50:06 -0700

    check

--- src/app.js
+++ src/app.js
@@ -14,9 +14,14 @@ app.set('view engine', 'ejs');
 //Configure the Static Directory
 app.use(express.static(path.join(__dirname, '/public')));
 
-const accountData = fs.readFileSync('src/json/accounts.json', {encoding:'utf8'});
-const accounts = JSON.parse(accountData)
 
+//read account data
+const accountData = fs.readFileSync('src/json/accounts.json', {encoding:'utf8'});
+const accounts = JSON.parse(accountData);
+
+//read user data
+const userData = fs.readFileSync('src/json/users.json', {encoding:'utf8'});
+const users = JSON.parse(userData);
 
 // Create the Index Route
 app.get('/', (req, res) => {
